3b807b52272aea6795d12eb6660978e11acf5d41
Kitae Park <pkt0615@naver.com>
2022-01-06 09:44:49 +0900

    WIP

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
+const passport = require("passport");
 
 app = express();
 port = 3000;
+
+require('./init/db')();
+require('./init/auth')();
 
 nunjucks.configure('views', {
     autoescape: false,
@@ -11,8 +15,10 @@ nunjucks.configure('views', {
 });
 
 app.use(express.static('static'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({ secret: "fuck", resave: false, saveUninitialized: false }));
+app.use(passport.authenticate('session'));
 
 const route = require('./routes/index');
 app.use('/', route);
